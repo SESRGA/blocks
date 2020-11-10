b4a8c0842b5ccfa89d5376d95f4c8d9e2a1c5fcb
user <sergeipanin111@gmail.com>
2020-11-10 15:46:26 +0200

    project is ready

---
+++ src/components/elem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div
+    class="block"
+    :class="selectBgColor"
+    @click="decrease()"
+  >
+    {{count}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "elem.vue",
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  computed: {
+    selectBgColor() {
+      if(this.count == 0) {
+        return "brColorRed"
+      }
+      return "brColorBlue"
+    }
+  },
+  created() {
+    this.count = Math.ceil(Math.random()*10)
+  },
+  methods: {
+    decrease() {
+      if(this.count != 0)
+        this.count--
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .block {
+    height: 100%;
+    color: white;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .brColorBlue {
+    background-color: blue;
+  }
+  .brColorRed {
+    background-color: red;
+  }
+</style>
---
+++ src/components/mainTable.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+      id="container">
+    <div
+      v-for="item in items"
+      :key="item"
+      style="margin: 2%"
+    >
+      <elem />
+    </div>
+  </div>
+</template>
+
+<script>
+import elem from './elem.vue'
+
+export default {
+  name: "mainTable.vue",
+  components: {
+    elem,
+  },
+  data() {
+    return {
+      items: 9,
+    }
+  },
+
+
+}
+</script>
+
+<style scoped>
+#container {
+  margin: 0;
+  display: grid;
+  height: 100vh;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+}
+</style>
